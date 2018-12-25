4be663b1e957ccb33ddb6ee74b0d50640ccffd5a
yoshi <yoppy1125yoppy@gmail.com>
2018-12-25 12:49:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def main():
         return render_template("markov.html", mark=test3.mar(request.form["userid"]))
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True)
